2f7efd08ec4c59876fc8cd7b48abcdb0e14f82a7
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-01 14:30:19 +0100

    Feat: main settings

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+
+    return (
+        <header className="py-20 flex">
+            <h1>This is a title</h1>
+        </header>
+    );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Main({ languages }) {
+ 
+    
+  return (
+    <main> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, officia rerum unde quos incidunt laborum reprehenderit numquam harum natus ex tempora quia inventore ipsum, nemo, itaque amet quod atque doloribus!  
+    
+    </main>
+  );
+}
